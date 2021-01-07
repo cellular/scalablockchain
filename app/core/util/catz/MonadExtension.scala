6ac39e435646e9ebc0e485525ddf6d0737734bc4
Ronny Wels <rwels@cellular.de>
2021-01-07 10:30:05 +0100

    formatting

--- app/core/util/catz/MonadExtension.scala
+++ app/core/util/catz/MonadExtension.scala
@@ -6,8 +6,8 @@ object MonadExtension {
 
   def foldOptionM[F[_], T](fa: F[Option[T]], fb: => F[T])(implicit m: Monad[F]): F[T] =
     Monad[F].flatMap(fa) {
-    case Some(v) => m.pure(v)
-    case _       => fb
-  }
+      case Some(v) => m.pure(v)
+      case _       => fb
+    }
 
 }
